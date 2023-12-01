807051381929904a264316b588ec577cc51aa29c
0lston <figuigfree1@gmail.com>
2023-12-01 03:53:06 +0100

    error fixed

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-
 from string import ascii_uppercase
-if __name__ == "__main__":
-    print(ascii_uppercase)
+print(ascii_uppercase)
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,9 +7,9 @@ if __name__ == "__main__":
     args_nbr = len(argv) - 1
 
     if args_nbr == 1:
-        print("{:d} argument:".format(args_nbr))
+        print("{} argument:".format(args_nbr))
     else:
-        print("{:d} arguments:".format(args_nbr))
+        print("{} arguments:".format(args_nbr))
 
     for i in range(1, args_nbr + 1):
         print("{}: {}".format(i, argv[i]))
