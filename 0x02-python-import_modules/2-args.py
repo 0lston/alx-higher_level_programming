1062807162b6fff301dafeb9cb7a46bb18c3af4e
0lston <figuigfree1@gmail.com>
2023-12-01 03:57:52 +0100

    error fixed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
 
     if args_nbr == 1:
         print("{} argument:".format(args_nbr))
-    if argc == 0:
-        print("{} arguments.".format(argc))
+    if args_nbr == 0:
+        print("{} arguments.".format(args_nbr))
     else:
         print("{} arguments:".format(args_nbr))
 
